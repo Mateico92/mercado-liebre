2135bf7cff30e6de19b37fbee5f284e35020cecd
mateico92 <mati_2012@live.com>
2024-02-27 20:55:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = requiere('express');
+const path = requiere('path');
+
+const app = express();
+
+app.use( express.static(path.resolve(__dirname, './public')) );
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
