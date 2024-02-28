e63e1d755a87e5ba6c66bcc2a97329128be19900
mateico92 <mati_2012@live.com>
2024-02-27 23:44:41 -0300

    asdf

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use( express.static(path.resolve(__dirname, './public')) );
 
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+const port = process.env.PORT || 3033;
+app.listen (port, () => 
+    console.log('Servidor corriendo en Puerto ${port}')
+);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
