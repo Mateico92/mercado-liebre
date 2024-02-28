81d2867518f78030fa832e03636c4d73232548e9
mateico92 <mati_2012@live.com>
2024-02-28 00:48:08 -0300

    asdfsdf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = requiere('express');
-const path = requiere('path');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +7,7 @@ app.use( express.static(path.resolve(__dirname, './public')) );
 
 const port = process.env.PORT || 3033;
 app.listen (port, () => 
-    console.log('Servidor corriendo en Puerto ${port}')
+    console.log('Servidor corriendo en Puerto${port}')
 );
 
 app.get('/', (req, res) => {
